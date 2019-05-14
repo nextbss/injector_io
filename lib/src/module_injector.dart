da13aac74f35ce40ff5a72eccb410c7ea83ef8c8
Pedro Massango <pedro.massango@nextbss.co.ao>
2019-05-14 22:23:53 +0100

    removed get() from module injector

--- lib/src/module_injector.dart
+++ lib/src/module_injector.dart
@@ -1,14 +1,12 @@
 library module_injector;
 
 import 'package:injectorio/src/models.dart';
-import 'package:injectorio/src/io.dart' show registry;
 
 abstract class Module {
   List<Definition> _moduleDef = [];
 
   void single<T>(T def) => _moduleDef.add( Definition<T>(Kind.SINGLE, () => def));
   void factory<T>(T def, DefBuilder<T> db) => _moduleDef.add( Definition<T>(Kind.FACTORY, db));
-  T get<T>() => registry.get<T>();
 
   List<Definition> mDef() => _moduleDef;
 }
